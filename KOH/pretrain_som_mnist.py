99a5a5b65c717ec0eb5311c822e1e48b0dd93611
Tymoteusz Kwiecinski <tkk2002tkk@gmail.com>
2024-05-10 19:27:24 +0000

    pretraining nets

--- KOH/pretrain_som_mnist.py
+++ KOH/pretrain_som_mnist.py
@@ -41,17 +41,31 @@ from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
 
 
-X = mnist.data
-y = mnist.target
+X = mnist.data.values
+y = mnist.target.values
 
 
-idx = cross_val(X, y, cv=5, epochs=15, M=10, N=10, grid = "rectangular", verbose=False, save_path='som_snapshots/net10x10/mnist_som')
+print("Evaluating net 10x10")
+idx = cross_val(X, y, cv=5, epochs=30, M=10, N=10, grid = "rectangular", verbose=True, save_path='som_snapshots/net10x10/rect/mnist_som')
 # save the indices
 np.save('som_snapshots/net10x10/mnist_som_rect_idx.npy', idx)
 
-
-idx = cross_val(X, y, cv=5, epochs=15, M=25, N=25, grid = "rectangular", verbose=False, save_path='som_snapshots/net25x25/mnist_som')
+print("Evaluating net 25x25")
+idx = cross_val(X, y, cv=5, epochs=30, M=25, N=25, grid = "rectangular", verbose=True, save_path='som_snapshots/net25x25/rect/mnist_som')
 np.save('som_snapshots/net25x25/mnist_som_rect_idx.npy', idx)
 
-idx = cross_val(X, y, cv=5, epochs=15, M=10, N=10, grid = "rectangular", neighbouring_func=MinusOneGaussianNeighboringFunc(initial_neighbouring_radius=0.3), lambda_param = 10, verbose=False, save_path='som_snapshots/net10x10/mnist_som_minusone')
+
+
+print("Evaluating net with hexagon grid")
+idx = cross_val(X, y, cv=5, epochs=30, M=10, N=10, grid = "hexagon", verbose=True, save_path='som_snapshots/net10x10/hex/mnist_som')
+np.save('som_snapshots/net10x10/mnist_som_minusone_rect_idx.npy', idx)
+
+
+print("Evaluating net with hexagon grid 25x25")
+idx = cross_val(X, y, cv=5, epochs=30, M=25, N=25, grid = "hexagon", verbose=True, save_path='som_snapshots/net25x25/hex/mnist_som')
+np.save('som_snapshots/net10x10/mnist_som_minusone_rect_idx.npy', idx)
+
+
+print("Evaluating net with minus derivative")
+idx = cross_val(X, y, cv=5, epochs=30, M=10, N=10, grid = "rectangular", neighbouring_func=MinusOneGaussianNeighboringFunc(initial_neighbouring_radius=0.3), lambda_param = 10, verbose=True, save_path='som_snapshots/net10x10/mnist_som_minusone')
 np.save('som_snapshots/net10x10/mnist_som_minusone_rect_idx.npy', idx)
